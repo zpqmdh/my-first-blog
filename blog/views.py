6bef8d551dd82299383dd134641aa6ee3779967a
zpqmdh <ysys1130@gmail.com>
2022-08-02 15:42:58 +0900

    second commit: Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    # render 함수를 호출하여 blog/post_list.html 템플릿을 보여줌
