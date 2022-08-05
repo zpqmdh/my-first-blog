a9af4de4039ba7354d6ce4f8f05e32d252cdcd75
zpqmdh <ysys1130@gmail.com>
2022-08-05 20:02:15 +0900

    fourth commit: Added view and template for detailed blog post as well as CSS for the site

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from time import timezone
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 # Create your views here.
@@ -8,3 +8,6 @@ def post_list(request):
     # posts: queryset name
     return render(request, 'blog/post_list.html', {'posts': posts})
     # render 함수를 호출하여 blog/post_list.html 템플릿을 보여줌
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
